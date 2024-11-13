fb04ad652d5d7d7900c6cdaf49cd9b64fe42d915
Коммерческие курсы <comcuru@teach.local>
2024-11-13 12:37:06 +0300

    code_of_a_fool

---
+++ 4th_point_13_11/4th_point_13_11/AddUsersWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4th_point_13_11
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUsersWindow.xaml
+    /// </summary>
+    public partial class AddUsersWindow : Window
+    {
+        public AddUsersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ 4th_point_13_11/4th_point_13_11/classes/FrameClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4th_point_13_11.classes
+{
+    class FrameClass
+    {
+
+    }
+}
